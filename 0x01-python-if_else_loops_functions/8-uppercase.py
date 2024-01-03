64db7f6f158f6f4e5fc59af2a1617c9cbf527b34
nathen321 <walidjalaly43@gmail.com>
2024-01-03 15:51:16 -0800

    ok

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    newone = ""
+    for i in range(len(str))
+        if ord(str[i]) > 96 and ord(str[i]) < 123:
+            newone += chr(ord(i) - 32)
+        else:
+            newone += str[i]
+    print(newone)
