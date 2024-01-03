30e79d1bcfd09fe12292114d9fd67c04f5106e67
nathen321 <walidjalaly43@gmail.com>
2024-01-03 13:51:22 -0800

    ok

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,6 @@ for x in range(10):
     for y in range(10):
         if x == 8 and y == 9:
             print("89")
+            break
         if y > x:
             print("{:d}{:d}, ".format(x, y), end = "")
