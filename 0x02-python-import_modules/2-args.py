10dbf2eb0e2dd43209e1d4da32d14ca97aa45c6e
nathen321 <walidjalaly43@gmail.com>
2024-01-04 14:49:49 -0800

    ok

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,9 @@ import sys
 
 lise = sys.argv
 siz = len(lise) - 1
-i = 1
 
 if siz == 0:
-    print("0 arguments.")
+    print(siz, "arguments.")
 elif siz> 0:
     print("{:d} arguments:".format(siz))
     for i in range(1, siz + 1):
