9030b597366ba96c2d90c12481f76aa884fe978b
nathen321 <walidjalaly43@gmail.com>
2024-01-04 14:56:34 -0800

    ok

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,11 @@ siz = len(lise) - 1
 if __name__ == "__main__":
     if siz == 0:
         print(siz, "arguments.")
-    elif siz> 0:
+    elif siz == 1:
+        print("{:d} argument:".format(siz))
+        for i in range(1, siz + 1):
+            print("{:d}: {}".format(i, lise[i]))
+    elif siz > 1:
         print("{:d} arguments:".format(siz))
         for i in range(1, siz + 1):
             print("{:d}: {}".format(i, lise[i]))
