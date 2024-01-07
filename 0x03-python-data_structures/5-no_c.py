b51242ae0d53443e4a935cc93a3cc3d9c170b1ca
nathen321 <walidjalaly43@gmail.com>
2024-01-07 10:07:31 -0800

    ok

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     new_string = ""
     for x in my_string:
         if x != "c" and x != "C":
-            new_string.append(x)
+            new_string += x
 
     return new_string
