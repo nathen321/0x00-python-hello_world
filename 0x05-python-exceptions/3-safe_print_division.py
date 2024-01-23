c197c21dfe1a8ca76d226d95f4f28924e5fd4ee5
nathen321 <walidjalaly43@gmail.com>
2024-01-22 18:40:32 -0800

    exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -9,14 +9,3 @@ def safe_print_division(a, b):
         print("Inside result: {}".format(tor))
     finally:
         return tor
-
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
