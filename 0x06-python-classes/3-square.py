4228f8e9ac9a14e25fc6a55d6bbcb37bf10d7ec2
nathen321 <walidjalaly43@gmail.com>
2024-01-23 16:24:36 -0800

    exception

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -26,3 +26,21 @@ class Square:
     """
     def area(self):
         return self._Square__size ** 2
+
+
+
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
