a6f5f50ee28280e3141746079551bf68b9b20c3b
nathen321 <walidjalaly43@gmail.com>
2024-01-23 16:22:11 -0800

    exception

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Square Class
+
+just a square
+
+"""
+
+
+class Square:
+    """ init a square
+
+    like i said a square
+
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+    
+    """ get area of a square
+
+    like i said area of a square
+
+    """
+    def area(self):
+        return self._Square__size ** 2
