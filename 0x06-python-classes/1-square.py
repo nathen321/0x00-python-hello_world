f3f9d0efca2d0f64f4648f4295e944e92660b0de
nathen321 <walidjalaly43@gmail.com>
2024-01-23 16:08:55 -0800

    exception

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square Class
+
+just a square
+
+"""
+
+
+class Square:
+    """ square clss
+
+    like i said a square
+
+    """
+    def __init__(self, size):
+        self._Square__size = size
