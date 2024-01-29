7c641e3dc05e6c716b6cd117d5d6813bd6eb3077
nathen321 <walidjalaly43@gmail.com>
2024-01-29 15:43:07 -0800

    exception

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,9 +20,8 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
