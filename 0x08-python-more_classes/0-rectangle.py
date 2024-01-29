795464b780035f3f75851d3863cdc3665c971afc
nathen321 <walidjalaly43@gmail.com>
2024-01-29 14:37:21 -0800

    exception

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -7,10 +7,6 @@ A module with a Rectangle that does nothing
 
 
 class Rectangle:
-        """
-
-        An empty Rectangle class
-
-        """
+        """ it's a rectangle pls work """
 
         pass
