44e306be660beedb538f4ecde7550658208e895b
nathen321 <walidjalaly43@gmail.com>
2024-01-29 18:21:27 -0800

    exception

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -71,6 +71,17 @@ class Rectangle:
             return 0
         return (2 * self.__height) + (2 * self.__width)
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """comparing to rec"""
+        if isinstance(rect_1, Rectangle) is False:
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if isinstance(rect_2, Rectangle) is False:
+            raise TypeError('rect_2 must be an instance of Rectangle')
+        if rect_1.area() > rect_2.area():
+            return rect_1
+        return rect_2
+
     def __str__(self):
         """draw"""
         rect_str = ''
