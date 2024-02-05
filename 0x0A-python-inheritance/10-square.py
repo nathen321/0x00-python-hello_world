720ff6d869742dfeb960c99b661f0596ef7987fa
nathen321 <walidjalaly43@gmail.com>
2024-02-05 14:49:38 -0800

    exception

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -50,5 +50,6 @@ class Square(Rectangle):
         Args:
             size: ....
         """
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
