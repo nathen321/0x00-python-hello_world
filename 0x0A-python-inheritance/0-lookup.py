6836ee5783c679fd3d351975a0e0a6371925e82c
nathen321 <walidjalaly43@gmail.com>
2024-02-05 09:42:29 -0800

    exception

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""function that returns the list of available attributes and methods of an object"""
 def lookup(obj):
-    """
-    function that returns the list of available attributes and methods of an object
+    """function that returns the list of available attributes and methods of an object.
+    Args:
+        obj is a class
+    Returns:
+        list: of attributes
     """
     return dir(obj)
