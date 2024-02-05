6f89e2e06af26269abf7939a26c6442c3d61cac4
nathen321 <walidjalaly43@gmail.com>
2024-02-05 10:08:11 -0800

    exception

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,11 +2,12 @@
 """Exact same object."""
 
 def is_same_class(obj, a_class):
-    """returns the list of available attributes and methods of an object.
+    """see if obj is a subclasse of a_class
     Args:
         obj: is a class
+        a_class: annother class
     Returns:
-        list: of attributes
+        True or False
     """
     if issubclass(obj, a_class):
         return True
