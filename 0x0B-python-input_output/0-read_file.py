35a759f2e33d39388639c24f3f73f537ebe8c4e5
nathen321 <walidjalaly43@gmail.com>
2024-02-06 14:31:18 -0800

    exception

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, 'r', encoding="utf-8") as fl:
+    with open(filename, encoding="utf-8") as fl:
         for line in fl:
             print(fl)
