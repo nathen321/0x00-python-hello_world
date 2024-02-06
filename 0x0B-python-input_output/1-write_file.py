6d8b59c007d421ddf1573b93bdb2e50b83157544
nathen321 <walidjalaly43@gmail.com>
2024-02-06 14:48:56 -0800

    exception

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,5 +2,5 @@
 """read file"""
 def write_file(filename="", text=""):
     """reads files"""
-    with open(filename, "w", encoding="utf-8") as fl:
-        fl.write(text)
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
