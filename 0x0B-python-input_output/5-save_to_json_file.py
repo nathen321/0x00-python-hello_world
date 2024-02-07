6a516f51b74b2cbda6be9c979ae932626b1e60fb
nathen321 <walidjalaly43@gmail.com>
2024-02-06 16:52:15 -0800

    exception

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that returns the JSON representation of an object (string)"""
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """" representation """
+    with open(filename, 'w', encodin="utf-8") as fl:
+        json.dump(my_obj, fl)
