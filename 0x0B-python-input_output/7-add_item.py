7728458df8f2c183b818b1843f8f3af305a9f779
nathen321 <walidjalaly43@gmail.com>
2024-02-07 16:14:50 -0800

    exception

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+from os import path
+load_json = __import__('6-load_from_json_file').load_from_json_file
+save_json = __import__('5-save_to_json_file').save_to_json_file
+
+if path.exists('add_item.json'):
+    json_file = load_json('add_item.json')
+else:
+    json_file = []
+
+for i range(1, len(argv)):
+    json_file.append(argv[i])
+
+save_json(json_file, 'add_item.json')
