f19ea1d35ae53c683877d3327bd20dfd30df8745
nathen321 <walidjalaly43@gmail.com>
2024-02-07 16:55:38 -0800

    exception

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''ex: 9 work in progress'''
+
+class Student:
+    """norma student"""
+    def __init__(self, first_name, last_name, age):
+        """initiaisation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retyvin the dict"""
+        dic = self.__dict__
+        seld = dict()
+
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return dic
+                    
+                if attr in dic:
+                    seld[attr] = dic[attr]
+
+            return seld
+        return dic
