f9251387c03fbe2b34e59bd5a1f780b6107e6abf
nathen321 <walidjalaly43@gmail.com>
2024-04-14 11:50:50 -0700

    exception

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,9 +5,12 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name\
+                LIKE BINARY 'N%' ORDER BY id ASC")
+    for state in cur.fetchall():
+        if state[1][0] == "N":
+            print(state)
 
     cur.close()
     db.close()
