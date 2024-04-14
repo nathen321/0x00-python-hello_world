c2f3007a9b083b4783e4b54f3d404d30f060ca7a
nathen321 <walidjalaly43@gmail.com>
2024-04-14 14:10:24 -0700

    exception

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,5 +13,5 @@ class State(Base):
     """
 
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
