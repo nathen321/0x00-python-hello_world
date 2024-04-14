6a3e9ce02904f6c2672aa67d454201b20524b4c1
nathen321 <walidjalaly43@gmail.com>
2024-04-14 11:41:13 -0700

    exception

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,6 +6,6 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states`")
+    cur.execute("SELECT * FROM `states` ORDER BY `id`")
     rows = cur.fetchall()
     [print(state) for state in rows if state[1][0] == 'N']
