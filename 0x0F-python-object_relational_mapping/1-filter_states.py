c1d63aa5e02160a3c1b31e507d38d50e95e4b458
nathen321 <walidjalaly43@gmail.com>
2024-04-14 11:39:44 -0700

    exception

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states`")
+    rows = cur.fetchall()
+    [print(state) for state in rows if state[1][0] == 'N']
