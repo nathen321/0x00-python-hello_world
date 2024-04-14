ecc30e74175864564929de8393c59fa67654f1e9
nathen321 <walidjalaly43@gmail.com>
2024-04-14 14:07:44 -0700

    exception

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,8 +5,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
-    """ file that contains the class definition of a State and an instance Base = declarative_base()"""
+    """
+    file that contains the class definition of a State and
+    an instance Base = declarative_base()
+    """
+
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
