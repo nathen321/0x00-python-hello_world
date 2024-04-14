f472cb30f3d457fe4b7093ffd559900663cc826b
nathen321 <walidjalaly43@gmail.com>
2024-04-14 11:58:31 -0700

    exception

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,1 @@
-#!/usr/bin/python3
 
-import sys
-import MySQLdb
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-            host="localhost", port=3306, user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name\
-                LIKE BINARY 'N%' ORDER BY id ASC")
-
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-
-    cur.close()
-    db.close()
