5194e72f236ac9e370b49fea96c1e0c464d6ccc6
nathen321 <walidjalaly43@gmail.com>
2024-04-14 12:28:31 -0700

    exception

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Write a script that takes in an argument and displays all values
+"""
+import MySQLdb
+from sys import argv
+
+# The code should not be executed when imported
+if __name__ == '__main__':
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
+
+    cur.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY %s", argv[4])
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
