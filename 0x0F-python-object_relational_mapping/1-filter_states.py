32200b6fa4a1999e3c7a9781e30ca3704fd7eda8
nathen321 <walidjalaly43@gmail.com>
2024-04-14 11:43:46 -0700

    exception

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
-from sys import argv
+# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+# Usage: ./1-filter_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM `states` ORDER BY `id`")
-    rows = cur.fetchall()
-    [print(state) for state in rows if state[1][0] == 'N']
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
