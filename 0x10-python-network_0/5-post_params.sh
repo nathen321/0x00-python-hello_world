d9a1a9f70ef8b07583c39f7fb6a0c81dcc0264d9
nathen321 <walidjalaly43@gmail.com>
2024-04-25 17:41:28 -0700

    exception

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the respons
-curl -sX POST $1 -H "email=test@gmail.com&subject=I will always be here for PLD" -L
+curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L
