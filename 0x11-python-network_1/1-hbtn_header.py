e62904f0523df7822954bff8941ad3986898ee4c
nathen321 <walidjalaly43@gmail.com>
2024-04-26 18:02:29 -0700

    exception

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,4 +7,4 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
-        print(dict(response.headers).get("X-Request-Id"))     
+        print(dict(response.headers).get("X-Request-Id"))
