8e9b05c71f13628cc97ac67cec44de8f9db28418
nathen321 <walidjalaly43@gmail.com>
2024-04-26 19:06:16 -0700

    exception

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    r = requests.get(sys.argv[1])
+    code = r.status_code
+    if code >= 400:
+        print("Error code: ", code)
+    else:
+        print(r.text)
