5881fd6b701414442d4fdc39886549f8b87adf15
nathen321 <walidjalaly43@gmail.com>
2024-04-26 17:52:54 -0700

    exception

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-#Python script that fetches https://alx-intranet.hbtn.io/status
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
 
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    rep = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(rep)))
-    print("\t- content: {}".format(rep))
-    print("\t- utf8 content: {}".format(rep.decode("utf-8")))
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        rep = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(rep)))
+        print("\t- content: {}".format(rep))
+        print("\t- utf8 content: {}".format(rep.decode("utf-8")))
