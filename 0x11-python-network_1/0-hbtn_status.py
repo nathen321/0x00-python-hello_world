09b3595149903efad1fa7f186766502c97039dc2
nathen321 <walidjalaly43@gmail.com>
2024-04-26 18:00:37 -0700

    exception

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         rep = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(rep)))
