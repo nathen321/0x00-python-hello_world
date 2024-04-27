796145b5e245c72294425aba24f6005433a8601b
nathen321 <walidjalaly43@gmail.com>
2024-04-26 17:58:19 -0700

    exception

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-#Python script that fetches https://alx-intranet.hbtn.io/status
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
 
 import urllib.request
 import sys
 
-url = sys.argv[1]
-
-with urllib.request.urlopen(url) as response:
-    print(dict(response.headers).get("X-Request-Id"))     
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(dict(response.headers).get("X-Request-Id"))     
