307cc72c5f7b3142e1b6fe5363d75c21888ed74f
nathen321 <walidjalaly43@gmail.com>
2024-04-28 12:19:45 -0700

    exception

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,13 +2,12 @@
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 
 import requests
-from requests.auth import HTTPBasicAuth
 import sys
 
 if __name__ == "__main__":
     user = sys.argv[1]
     pw = sys.argv[2]
-    basic = HTTPBasicAuth(user, pw)
+    basic = requests.auth.HTTPBasicAuth(user, pw)
 
     r = requests.get(
             "https://api.github.com/users/{}".format(user), auth=basic)
