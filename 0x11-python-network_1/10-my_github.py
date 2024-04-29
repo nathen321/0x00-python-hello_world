d8b9c774f64588053bc6e584c64875dc80911ab2
nathen321 <walidjalaly43@gmail.com>
2024-04-28 17:41:54 -0700

    exception

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,10 +5,6 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    pw = sys.argv[2]
-    basic = requests.auth.HTTPBasicAuth(user, pw)
-
-    r = requests.get(
-            "https://api.github.com/user", auth=basic)
-    print(r.json().info.get('id'))
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
