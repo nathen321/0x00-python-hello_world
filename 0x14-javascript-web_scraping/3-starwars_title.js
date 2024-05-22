f91264b959537b58b1af7bb12d7123fa5a1cb4cc
nathen321 <walidjalaly43@gmail.com>
2024-05-22 02:51:35 -0700

    exception

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-let url = "https://swapi-api.alx-tools.com/api/films/:id";
-url = url.replace(":id" , process.argv[2]);
+let url = 'https://swapi-api.alx-tools.com/api/films/:id';
+url = url.replace(':id', process.argv[2]);
 request(url, function (error, response, body) {
+  if (error) console.error('error:', error);
   console.log(JSON.parse(body).title);
 });
